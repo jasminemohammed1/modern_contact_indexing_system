b941e8ad18b80f5cd5b9d738e52154194d9a1880
jasmine <jasminemohamed174005@gmail.com>
2026-02-22 11:15:01 +0200

    Implement the storage interface

---
+++ modern_contact_indexing_system/IStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modern_contact_indexing_system
+{
+    internal interface IStorage
+    {
+        void Save(List<Contact> contacts);
+        List<Contact> LoadContacts();
+        
+
+        
+    }
+}
